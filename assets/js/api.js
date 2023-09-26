469deeb32740a724ab671fd216ddf0651bfd3319
MariaAbreu <mariaeduardaabreu005@gmail.com>
2023-09-25 21:03:31 -0300

    A2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = "";
+    const url = "https://raw.githubusercontent.com/MariaAbreu005/portifolio/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
