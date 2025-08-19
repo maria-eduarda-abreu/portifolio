8d637f7e867b20238a1912c9bff25c0f3b351352
unknown <mariaeduardaabreu005@gmail.com>
2025-08-19 18:56:19 -0300

    passando caminho local para api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,6 @@
-
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/maria-eduarda-abreu/portifolio/main/data/profile.json";
+    const url = "data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
-
