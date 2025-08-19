646fd9e8133b368881537269950dba52a1250aa4
unknown <mariaeduardaabreu005@gmail.com>
2025-08-19 17:32:16 -0300

    atualizando a url que busca o arquivo JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/MariaAbreu005/portifolio/main/data/profile.json";
+    const url = "https://raw.githubusercontent.com/maria-eduarda-abreu/portifolio/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
